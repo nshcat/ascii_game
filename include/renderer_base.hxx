ff3eff5ccd4f2fd6fb2d5fdb863ed93f1b06359c
nshcat <nshcat@users.noreply.github.com>
2016-11-04 22:58:03 +0100

    Added possibility to create renderer with vsync turned on.

--- include/renderer_base.hxx
+++ include/renderer_base.hxx
@@ -10,19 +10,20 @@ namespace ascii
 	class renderer_params
 	{
 		public:
-			renderer_params(dimensions p_WndSz, dimensions p_Glyphs, std::string p_Title, std::string p_TexPath)
+			renderer_params(dimensions p_WndSz, dimensions p_Glyphs, std::string p_Title, std::string p_TexPath, bool p_Vsync)
 				: 	m_WndW(p_WndSz.w()),
 					m_WndH(p_WndSz.h()),
 					m_Title(std::move(p_Title)),
 					m_TexPath(std::move(p_TexPath)),
 					m_GlyphsX(p_Glyphs.w()),
-					m_GlyphsY(p_Glyphs.h())
+					m_GlyphsY(p_Glyphs.h()),
+					m_Vsync(p_Vsync)
 			{
 				
 			}
 			
-			renderer_params(dimensions p_WndSz, std::string p_Title, std::string p_TexPath)
-				: 	renderer_params(p_WndSz, { 16, 16 }, std::move(p_Title), std::move(p_TexPath))
+			renderer_params(dimensions p_WndSz, std::string p_Title, std::string p_TexPath, bool p_Vsync)
+				: 	renderer_params(p_WndSz, { 16, 16 }, std::move(p_Title), std::move(p_TexPath), p_Vsync)
 			{
 				
 			}
